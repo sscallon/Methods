82607dfafb0daaf12c01b7767e7c41f85242de49
Shane Scallon <sscallon@usf.edu>
2020-02-06 12:14:52 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ask the user to enter their name
+            Console.WriteLine("Please enter your first and last name...");
+
+            try
+            {
+
+                string input = Console.ReadLine();
+
+                string inputValue = Full_Name(input);
+               
+
+
+
+            }//end of try
+            catch
+            {
+                Console.WriteLine("Please restart application and enter only first and last name");
+                Console.WriteLine("Press any button to exit");
+                Console.ReadKey(true);
+            }//end of catch
+        }
+        private static string Full_Name(string Get_Name)
+        {
+            Console.WriteLine("Hello " + Get_Name);
+
+            return Get_Name;
+
+            
+        }
+    }
+}
